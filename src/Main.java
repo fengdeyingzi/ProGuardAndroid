8f8cc15bee64bee5ad4c842c41c627889e2f22a1
2541012655@qq.com <2541012655@qq.com>
2018-06-07 17:44:57 +0800

    ..

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import proguard.ProGuard;
+
+public class Main
+{
+	
+	public static void main(String[] args)
+    {
+		String arays[] = new String[]{"@/storage/emulated/0/AppProjects/作品/proguard-master/examples/proguard.pro"};
+		
+		//调用proguard
+		ProGuard.main(arays);
+	}
+	
+	
+}
